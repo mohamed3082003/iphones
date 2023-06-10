30126b93917a235d1423e28e797df84b83c24bf9
mohamed mohsen <100736437+mohamed23134@users.noreply.github.com>
2023-06-10 18:45:41 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+
+let img = document.querySelector('.img');
+let container = document.querySelector('.container');
+function photos(photo){
+    img.src = photo;
+}
+function colors(color){
+    container.style.background = color; 
+
+}
